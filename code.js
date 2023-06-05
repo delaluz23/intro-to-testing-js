0d0dfee6d0b6a6596f158949f2b3f05ed09e39e1
Jose De La Luz <Jdelaluz01@gmail.com>
2023-06-05 12:07:15 -0500

    implemented isEven function with jasmin unit testing

--- code.js
+++ code.js
@@ -1,24 +1,35 @@
 // helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
-}
-function sayHello(x){
-    if(typeof(x) !== 'string'){
-        return "Hello, World!"
-    } else {
-        return (`Hello, ${x}!`);
-    }
-}
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+// function sayHello(x){
+//     if(typeof(x) !== 'string'){
+//         return "Hello, World!"
+//     } else {
+//         return (`Hello, ${x}!`);
+//     }
+// }
+//
+// function isFive(x){
+//     if(typeof x !== 'number'){
+//         return false
+//     }else{
+//         if(x === 5){
+//             return `${x} is 5`
+//         }
+//         else {
+//             return `${x} isn't 5`
+//         }
+//     }
+// }
 
-function isFive(x){
-    if(typeof x !== 'number'){
+function isEven(x){
+    if(typeof x == 'boolean'){
         return false
+    }
+    else if(x % 2 == 0){
+        return true;
     }else{
-        if(x === 5){
-            return `${x} is 5`
-        }
-        else {
-            return `${x} isn't 5`
-        }
+        return false;
     }
 }
--- tests.js
+++ tests.js
@@ -48,23 +48,58 @@
 
 //unit test for isFive function
 
-describe('isFive', function (){
+// describe('isFive', function (){
+//     it('should be a defined function', function () {
+//         expect(typeof isFive).toBe('function');
+//     });
+//     it('should return false w/ "number', function () {
+//         expect(isFive('number')).toBe(false);
+//     });
+//     it('should return false w/ null', function () {
+//         expect(isFive(null)).toBe(false);
+//     });
+//     it('should return false w/ true', function () {
+//         expect(isFive(true)).toBe(false);
+//     });
+//     it('should return "5 is 5"', function () {
+//         expect(isFive(5)).toBe("5 is 5");
+//     });
+//     it('should return "1 is 5"', function () {
+//         expect(isFive(1)).toBe("1 isn't 5");
+//     });
+// })
+
+
+//unit test for isEven Function
+describe('isEven', function (){
     it('should be a defined function', function () {
-        expect(typeof isFive).toBe('function');
+        expect(typeof isEven).toBe('function');
     });
-    it('should return false w/ "number', function () {
-        expect(isFive('number')).toBe(false);
+    it('should return true when executed with isEven(2)', function () {
+        expect(isEven(2)).toBe(true);
     });
-    it('should return false w/ null', function () {
-        expect(isFive(null)).toBe(false);
+    it('should return true when executed with isEven(-4)', function () {
+        expect(isEven(-4)).toBe(true);
     });
-    it('should return false w/ true', function () {
-        expect(isFive(true)).toBe(false);
+    it('should return false when executed with isEven(3)', function () {
+        expect(isEven(3)).toBe(false);
     });
-    it('should return "5 is 5"', function () {
-        expect(isFive(5)).toBe("5 is 5");
+    it('should return false when executed with isEven(bananas)', function () {
+        expect(isEven('bananas')).toBe(false);
     });
-    it('should return "1 is 5"', function () {
-        expect(isFive(1)).toBe("1 isn't 5");
+    it('should return true when executed with isEven("8")', function () {
+        expect(isEven("8")).toBe(true);
+    });
+    it('should return false when executed with isEven(infinity)', function () {
+        expect(isEven(Infinity)).toBe(false);
+    });
+    it('should return false when executed with isEven(false)', function () {
+        expect(isEven(false)).toBe(false);
+    });
+    it('should return false when executed with isEven(true)', function () {
+        expect(isEven(true)).toBe(false);
+    });
+    it('should return false when executed with isEven()', function () {
+        expect(isEven()).toBe(false);
     });
 })
